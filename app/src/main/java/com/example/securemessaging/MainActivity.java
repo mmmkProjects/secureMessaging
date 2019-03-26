ec4c0042f73cbacb4f27b6ffa4c081c39aa2ed9c
mmmkProjects <securemessaging45@gmail.com>
2019-03-26 20:42:42 +0530

    commit 26/3/19 8.28pm: firebase authentication added login verification works

--- app/src/main/java/com/example/securemessaging/MainActivity.java
+++ app/src/main/java/com/example/securemessaging/MainActivity.java
@@ -2,17 +2,29 @@ package com.example.securemessaging;
 
 import android.content.Intent;
 import android.graphics.Paint;
-import android.media.Image;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    EditText user,pass;
     ImageView img;
     Animation anim;
     Button forgot_pass;
@@ -21,16 +33,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
         img = (ImageView) findViewById(R.id.sharingan);
         anim = AnimationUtils.loadAnimation(this,R.anim.login);
         img.startAnimation(anim);
         forgot_pass = (Button)findViewById(R.id.forgot_password);
         forgot_pass.setPaintFlags(forgot_pass.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);//underlining the text
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
 
-    public void listOfMessages(View view) {
-        i = new Intent(this,listOfmessages.class);
-        startActivity(i);
 
+    }
+
+    public void signIn(String email,String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Log.d("Logged-in", "signInWithEmail:success");
+                            i = new Intent(MainActivity.this,listOfmessages.class);
+                            startActivity(i);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("Incorrect user and pass", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+    public void login(View view) {
+
+        user = (EditText)findViewById(R.id.username);
+        pass = (EditText)findViewById(R.id.password);
+        String userNameString = user.getText().toString();
+        String passwordString = pass.getText().toString();
+        if(TextUtils.isEmpty(userNameString)||TextUtils.isEmpty(passwordString)){
+            Toast.makeText(MainActivity.this,"please enter all the details",Toast.LENGTH_SHORT).show();
+        }
+        else
+            signIn(userNameString,passwordString);
     }
 }
